a89c93c74917e2c1aee0a28f1308bd2c7d540766
Martin Burkel <Martin.Burkel@ibm.com>
2025-03-14 14:50:23 +0100

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/real-estate-map',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
